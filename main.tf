cec71f5555f302132547bf65fd3848ab1774ea0f
Salomao123 <salomaum.batiata@gmail.com>
2023-09-05 22:46:52 -0300

    feat:
    Remove name attribute

--- main.tf
+++ main.tf
@@ -19,7 +19,6 @@ resource "aws_db_instance" "rds-banking" {
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
-  name                 = "banking"
   username             = "salomao"
   password             = "iO9ZCI9z/!"
   parameter_group_name = "default.mysql5.7"
